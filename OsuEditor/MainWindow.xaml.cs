c73c216534bd67ab05a1093ae0977d20cea41449
CloudHolic <bluegkstjr@kaist.ac.kr>
2018-03-18 19:26:29 +0900

    초기 커밋.

---
+++ OsuEditor/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace OsuEditor
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
