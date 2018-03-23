c8447f6e34dfe5a1b90c923f6032b8493d563864
CloudHolic <bluegkstjr@kaist.ac.kr>
2018-03-23 17:14:41 +0900

    주석 추가

--- OsuEditor/Controls/MusicBar.cs
+++ OsuEditor/Controls/MusicBar.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace OsuEditor.Controls
@@ -7,6 +8,11 @@ namespace OsuEditor.Controls
     [TemplatePart(Name = "PART_Line", Type = typeof(Line))]
     public class MusicBar : Slider
     {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
 
+            //TODO: Draw preview points, SV points, Kiai periods, break periods
+        }
     }
 }
